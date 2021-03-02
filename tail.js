231b4ae61a03b428c4a3930dc0a1d4395e9c1f56
leightonchien <leightonchien@gmail.com>
2021-03-02 14:22:42 +0000

    tail is every element except the head of array

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function tail(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+  
+module.exports = tail;
