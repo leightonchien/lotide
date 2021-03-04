8a07001d3d3f798644d6f32bba69dafb3e35c24a
leightonchien <leightonchien@gmail.com>
2021-03-04 07:14:42 +0000

    counts how many of each letter in a sentence

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+// const assertEqual = function(actual, expected) {
+//     if (actual === expected) {
+//       console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//     } else {
+//       console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//     }
+//   };
+
+const countLetters = function(sentence) {
+    let str = sentence.split(" ").join("");
+    const results = {};
+  
+    for (let i = 0; i < str.length; i++) {
+      if (results[str[i]]) {
+        results[str[i]] += 1;
+      } else {
+        results[str[i]] = 1;
+      }
+    }
+    console.log(results);
+    return results;
+  };
+    
+
+// TEST
+countLetters("lighthouse labs a beacon for career path");
