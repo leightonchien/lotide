7e9098d12365acb444b325ce7e04d7707cd31b0a
leightonchien <leightonchien@gmail.com>
2021-03-09 01:36:18 +0000

    refactor

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 function tail(array) {
   let newArray = [];
   for (let i = 1; i < array.length; i++) {
