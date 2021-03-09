48002fc894bfadc234a30355f21dbc8b2377268d
leightonchien <leightonchien@gmail.com>
2021-03-09 02:11:18 +0000

    refactor

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require("../assertArraysEqual");
+
+// TEST
+assertArraysEqual ([1, 2, 3], [1, 2, 3]); 
+assertArraysEqual ([1, 2, 3], [3, 2, 1]);
+assertArraysEqual ([1, 2, 3], [1, 2, '3']);
+assertArraysEqual ([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual (['',false, 0], [null, false, NaN]);
