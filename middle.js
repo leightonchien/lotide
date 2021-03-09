2ba0ecc44da2a6738398b350293d0598bac72ba4
leightonchien <leightonchien@gmail.com>
2021-03-09 02:15:44 +0000

    refactor

--- middle.js
+++ middle.js
@@ -43,13 +43,16 @@ const middle = function(array) {
   } return arrayResult;
 };
 
+
+module.exports = middle;
+
 //TEST//
 
-middle([1])
-middle([1, 2])
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
 
-middle([1, 2, 3])
-middle([1, 2, 3, 4, 5])
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 
-middle([1, 2, 3, 4])
-middle([1, 2, 3, 4, 5, 6])
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
